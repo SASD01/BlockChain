ad12c57c330fbc662bb8e1d32f59bd6e7318b647
ceisutb16 <ceisutb16@inv83877.utb.edu.co>
2022-03-04 09:29:00 -0500

    fixin login issue

--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
@@ -19,8 +19,8 @@ def singup(request):
         pass2 = request.POST['pass2']
 
         myuser = User.objects.create_user(username, email, pass1)
-        myuser.First_name = fname
-        myuser.Last_name = lname
+        myuser.first_name = fname
+        myuser.last_name = lname
     
         myuser.save()
 
@@ -34,16 +34,18 @@ def singup(request):
 def singin(request):
 
     if request.method == "POST":
-        username = request.POST["username"]
-        pass1 = request.POST["pass1"]
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
 
-        user = authenticate(username = username, pass1 = pass1)
+        print(username,pass1)
+
+        user = authenticate(username= username,password= pass1)
+        print(user)
 
         if user is not None:
             login(request, user)
-            fname = user.First_name
-            print(fname)
-            return redirect(request,'authentication/index.html', {'fname': fname})
+            fname = user.first_name
+            return render(request,'authentication/index.html', {'fname': fname})
         else:
             messages.error(request, "bad credentials")
             return redirect('home')
@@ -51,5 +53,8 @@ def singin(request):
     return render(request,"authentication/singin.html")
 
 def singout(request):
-    pass
+    logout(request)
+    messages.success(request,"u logged out succesfully")
+    return redirect('home')
+
 
